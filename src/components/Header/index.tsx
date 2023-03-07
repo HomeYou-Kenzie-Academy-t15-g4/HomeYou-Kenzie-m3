c0f324824b8089a410579332667c5648b9191e7f
inaciodh <diegohenriqueptc@hotmail.com>
2023-03-07 18:40:46 -0300

    update: header component done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import StyledHeader from './style';
+import imgLogo from '../../assets/logo.png';
 
 const Header = () => {
-  return <div></div>;
+  const navRef = React.useRef<HTMLElement | null>(null);
+
+  const showNavBar = () => {
+    navRef.current?.classList.toggle('responsive_nav');
+  };
+
+  return (
+    <StyledHeader>
+      <img src={imgLogo} alt='logo' />
+      <nav ref={navRef}>
+        <a href='#/'>Início</a>
+        <a href='#/'>Galeria</a>
+        <a href='#/'>Entrar</a>
+        <a href='#/'>Cadastro</a>
+        <button className='nav-btn nav-close-btn' onClick={showNavBar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <h3>user</h3>
+      <button className='nav-btn' onClick={showNavBar}>
+        <FaBars />
+      </button>
+    </StyledHeader>
+  );
 };
 
 export default Header;
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from '../../components/Header';
 
 const HomePage = () => {
-  return <div>azul</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default HomePage;
