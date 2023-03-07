214c2578a63724de2ed99550d0a7a3300a8257e3
inaciodh <diegohenriqueptc@hotmail.com>
2023-03-07 18:48:56 -0300

    fix: links are fixed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import StyledHeader from './style';
 import imgLogo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const navRef = React.useRef<HTMLElement | null>(null);
@@ -14,10 +15,10 @@ const Header = () => {
     <StyledHeader>
       <img src={imgLogo} alt='logo' />
       <nav ref={navRef}>
-        <a href='#/'>Início</a>
-        <a href='#/'>Galeria</a>
-        <a href='#/'>Entrar</a>
-        <a href='#/'>Cadastro</a>
+        <Link to={'/'}>Início</Link>
+        <Link to={'/house'}>Galeria</Link>
+        <Link to={'/login'}>Entrar</Link>
+        <Link to={'/register'}>Cadastro</Link>
         <button className='nav-btn nav-close-btn' onClick={showNavBar}>
           <FaTimes />
         </button>
