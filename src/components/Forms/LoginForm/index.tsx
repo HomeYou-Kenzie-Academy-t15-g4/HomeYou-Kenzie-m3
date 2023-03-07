c4af6c7292aa75cb3a3ee878ae76138c8eb01ea6
Gabriel Sales <gabrielsales081@gmail.com>
2023-03-07 10:56:24 -0300

    feat: add login form

--- src/components/Forms/LoginForm/index.tsx
+++ src/components/Forms/LoginForm/index.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { ILoginFormValue } from './types';
 
 const LoginForm = () => {
-  return <div></div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const submit: SubmitHandler<ILoginFormValue> = (formData) => {
+    // função login
+    reset();
+  };
+
+  return (
+    <form>
+      <fieldset>
+        <label>Email</label>
+        <input type='email' id='email' {...register('email')} />
+      </fieldset>
+      <fieldset>
+        <label>Senha</label>
+        <input type='password' id='password' {...register('password')} />
+      </fieldset>
+
+      <button type='submit'>Entrar</button>
+    </form>
+  );
 };
 
 export default LoginForm;
