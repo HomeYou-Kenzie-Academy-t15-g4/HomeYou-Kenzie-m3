ee7fe954b88e0a4d7562987f763fbe0649b31aec
guscarraro <gus.carraro@hotmail.com>
2023-03-08 14:56:27 -0300

    feat:Render Comments

--- src/pages/HousePage/index.tsx
+++ src/pages/HousePage/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import CommentsCard from '../../components/Cards/CommentsCard';
 
 const HousePage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <CommentsCard />
+    </div>
+  );
 };
 
 export default HousePage;
