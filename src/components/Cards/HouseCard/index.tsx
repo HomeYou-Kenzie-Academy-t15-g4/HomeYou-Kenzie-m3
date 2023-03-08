de4ed2f1b6317ea9725b309f1b63ddebaea6e243
guscarraro <gus.carraro@hotmail.com>
2023-03-08 11:26:07 -0300

    feat:Render  HouseCard

--- src/components/Cards/HouseCard/index.tsx
+++ src/components/Cards/HouseCard/index.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { HousesContext } from '../../../providers/HousesContext';
+import { IHouse } from '../../../providers/HousesContext/types';
 
 const HouseCard = () => {
-  return <div></div>;
+  const { housesList } = useContext(HousesContext);
+
+  return (
+    <div>
+      <ul>
+        <div>
+          {housesList.map((house: IHouse) => (
+            <li key={house.id} onClick={() => console.log(house.id)}>
+              <img src={house.photos[0]} alt='Image House' />
+              <div>
+                <p>
+                  <strong>
+                    {house.city}, {house.state}
+                  </strong>
+                </p>
+                <p>Alugar</p>
+              </div>
+            </li>
+          ))}
+        </div>
+      </ul>
+    </div>
+  );
 };
 
 export default HouseCard;
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import HouseCard from '../../components/Cards/HouseCard';
 import Header from '../../components/Header';
 
 const HomePage = () => {
   return (
     <div>
       <Header />
+      <HouseCard />
     </div>
   );
-import HouseForm from '../../components/Forms/HouseForm';
+  // import HouseForm from '../../components/Forms/HouseForm';
 
-const HomePage = () => {
-  return <>HomePage</>;
+  // const HomePage = () => {
+  //   return <>HomePage</>;
 };
 
 export default HomePage;
