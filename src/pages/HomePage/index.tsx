803f81e8d85892a2438fb93103e6a58683ce81cc
guscarraro <gus.carraro@hotmail.com>
2023-03-08 11:33:48 -0300

    fix:HouseCard Conflict

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import HouseCard from '../../components/Cards/HouseCard';
 import Header from '../../components/Header';
 
 const HomePage = () => {
   return (
     <div>
       <Header />
-      <HouseCard />
     </div>
   );
-  // import HouseForm from '../../components/Forms/HouseForm';
-
-  // const HomePage = () => {
-  //   return <>HomePage</>;
 };
 
 export default HomePage;
