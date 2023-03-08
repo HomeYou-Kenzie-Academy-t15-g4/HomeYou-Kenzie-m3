ab21bba9c32ad93646fde49e651f30dd7b54a839
inaciodh <diegohenriqueptc@hotmail.com>
2023-03-08 12:27:31 -0300

    update: modal done

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../../components/Header';
+import Modal from '../../components/Modal';
+import { UserContext } from '../../providers/UserContext';
 
 const HomePage = () => {
+  const { isOpen, setIsOpen } = useContext(UserContext);
+
   return (
     <div>
       <Header />
+      <button type='button' onClick={() => setIsOpen(true)}>
+        Abrir
+      </button>
+      {isOpen ? (
+        <Modal>
+          <div></div>
+        </Modal>
+      ) : null}
     </div>
   );
 };
--- src/pages/HousePage/index.tsx
+++ src/pages/HousePage/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import Header from '../../components/Header';
+import { UserContext } from '../../providers/UserContext';
 
 const HousePage = () => {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <button type='button' onClick={() => setIsOpen(true)}>
+        Abrir
+      </button>
+      {isOpen ? (
+        <Modal>
+          <div></div>
+        </Modal>
+      ) : null}
+    </div>
+  );
 };
 
 export default HousePage;
