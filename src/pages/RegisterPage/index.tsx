489f6429c587405e2fa5583bdfb0719f0ae93645
Gabriel Sales <gabrielsales081@gmail.com>
2023-03-10 09:59:28 -0300

    style: login page style: register page fix: register page

--- src/pages/RegisterPage/index.tsx
+++ src/pages/RegisterPage/index.tsx
@@ -1,18 +1,43 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import RegisterForm from '../../components/Forms/RegisterForm';
-import { StyledDivRegisterPage } from './style';
+import { StyledRegisterPage } from './style';
+import registerImg from '../../assets/register-asside-img.svg';
+import logo from '../../assets/HomeYou.svg';
+import { Link } from 'react-router-dom';
+import { StyledTitle } from '../../styles/typograthy';
 
 const RegisterPage = () => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleReSize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener('resize', handleReSize);
+    return () => window.removeEventListener('resize', handleReSize);
+  }, []);
+
+  const showSection = screenWidth > 900;
+
   return (
-    <StyledDivRegisterPage>
-      <section>
-        <img src='../../../src/assets/HomeYou.svg' alt='' />
+    <StyledRegisterPage>
+      <section className='form-section'>
+        <img src={logo} alt='' />
+        <div className='text-container'>
+          <Link to={'/login'}>
+            <StyledTitle $fontColor='grey' tag='h3' $fontSize='three'>
+              Voltar pro Login
+            </StyledTitle>
+          </Link>
+        </div>
         <RegisterForm />
       </section>
-      <aside>
-        <img src='../../../assets/register-asside-img.svg' alt='' />
-      </aside>
-    </StyledDivRegisterPage>
+      {showSection && (
+        <section className='image-section'>
+          <div>
+            <img src={registerImg} alt='' />
+          </div>
+        </section>
+      )}
+    </StyledRegisterPage>
   );
 };
 
