90ecc60efd37cd9d3da6e8e10bd5c4996e19bfa0
Gabriel Sales <gabrielsales081@gmail.com>
2023-03-11 02:17:16 -0300

    feat: add home page cards and style

--- src/components/Cards/HouseCard/index.tsx
+++ src/components/Cards/HouseCard/index.tsx
@@ -1,27 +1,41 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { HousesContext } from '../../../providers/HousesContext';
 import { IHouse } from '../../../providers/HousesContext/types';
+import { StyledButtonLink } from '../../../styles/button';
+import { StyledParagraph } from '../../../styles/typograthy';
+import { StyledList } from './style';
 
 const HouseCard = () => {
   const { housesList } = useContext(HousesContext);
 
   return (
     <div>
-      <ul>
+      <StyledList>
         {housesList.map((house: IHouse) => (
-          <li key={house.id} onClick={() => console.log(house.id)}>
-            <img src={house.photos[0]} alt='Image House' />
-            <div>
-              <p>
-                <strong>
+          <Link className='house-link' to={'/house'}>
+            {/* fiz uma 'gambiarra' com esse link enquanto o bug não resolve */}
+            <li key={house.id} onClick={() => console.log(house.id)}>
+              <div className='image-box'>
+                <img src={house.photos[0]} alt='Image House' />
+              </div>
+
+              <div className='text-box'>
+                <StyledParagraph $fontWeight='two' $fontColor='greyBold'>
                   {house.city}, {house.state}
-                </strong>
-              </p>
-              <p>Alugar</p>
-            </div>
-          </li>
+                </StyledParagraph>
+                <StyledParagraph $fontColor='grey' $fontWeight='three'>
+                  Alugar
+                </StyledParagraph>
+              </div>
+              {/* esse link esta bugado, não sei nem consegui entender o motivo, se alguém conseguir consertar */}
+              {/* <Link to={'/house'} className='link-house'>
+              ↗
+            </Link> */}
+            </li>
+          </Link>
         ))}
-      </ul>
+      </StyledList>
     </div>
   );
 };
