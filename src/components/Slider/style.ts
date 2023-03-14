9f325f62ff76a9d34f27578e6c2ba70a7a05ad84
Gabriel Maciel <gabrielc.maciel42@gmail.com>
2023-03-14 15:38:16 -0300

    swipper cs to styled-components

--- src/components/Slider/style.ts
+++ src/components/Slider/style.ts
@@ -3,51 +3,46 @@ import { Swiper } from 'swiper/react';
 
 const StyledSlider = styled(Swiper)`
   @media only screen and (max-width: 800px) {
-    .swiper-button-prev {
-      display: none;
-    }
-
-    .swiper-button-next {
-      display: none;
-    }
-
-    .swiper {
-      padding-bottom: 32px;
-      padding-top: 32px;
-    }
-  }
-
-  @media only screen and (min-width: 800px) {
-    .swiper {
-      padding: 32px;
-    }
-  }
-
-  .swiper-slide {
-    display: flex;
-    justify-content: center;
-    height: auto;
-  }
-
-  .swiper-pagination-bullet-active {
-    background-color: ${({ theme }) => theme.colors.grey200};
-  }
-
-  .swiper-button-disabled {
-    color: ${({ theme }) => theme.colors.grey100} !important;
-  }
-
-  .swiper-button {
-    color: ${({ theme }) => theme.colors.grey200} !important;
-  }
-
   .swiper-button-prev {
-    color: #718096;
+    display: none;
   }
 
   .swiper-button-next {
-    color: #718096;
+    display: none;
   }
+
+  &.swiper {
+    padding-bottom: 32px;
+    padding-top: 32px;
+  }
+}
+
+@media only screen and (min-width: 800px) {
+  &.swiper {
+    padding: 32px;
+  }
+}
+
+.swiper-slide {
+  display: flex;
+  justify-content: center;
+  height: auto;
+}
+
+.swiper-pagination-bullet-active {
+  background-color: #718096;
+}
+
+.swiper-button-prev {
+  left: -1px;
+  color: #718096;
+}
+
+.swiper-button-next {
+  right: 1px;
+  color: #718096;
+}
+
 `;
 
 export default StyledSlider;
